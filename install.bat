ba608c39cf4464ba434ca3498b565d7f6d3cffd0
n8nhahuytoai-tudong <n8nhahuytoai@gmail.com>
2025-11-11 08:52:05 +0700

    Delete install.bat

--- install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing dependencies...
-py -3.13 -m venv venv
-call venv\Scripts\activate
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-echo.
-echo Installation complete! Run: run.bat
-pause
